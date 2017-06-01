9b647a32aa62f863a40d5e960d6c1f51cf6e1183
andrewheusser <andrew.heusser@gmail.com>
2017-06-01 12:59:39 -0400

    added scaffolding for stream object

---
+++ examples/streaming.py
@@ -0,0 +1,8 @@
+# load library
+import hypertools as hyp
+
+# initialize stream object
+stream = hyp.Stream(port=3004, verbose=True)
+
+# start stream
+stream.start()
--- hypertools/plot/streaming.py
+++ hypertools/plot/streaming.py
@@ -1,1 +1,125 @@
-# streaming code will go here
+import json
+import sys
+import numpy as np
+import time
+import zmq
+
+class Interface:
+    def __init__(self, port=3000, verbose=False):
+        context = zmq.Context()
+        self._socket = context.socket(zmq.PAIR)
+        # self._socket.connect("tcp://localhost:%d" % port)
+        self._socket.connect("tcp://localhost:%d" % int(port))
+
+        self.verbose = verbose
+
+        if self.verbose:
+            print "Client Ready!"
+
+        # Send a quick message to tell node process we are up and running
+        self.send(json.dumps({
+            'action': 'started',
+            'command': 'status',
+            'message': time.time()*1000.0
+        }))
+
+    def send(self, msg):
+        """
+        Sends a message to TCP server
+        :param msg: str
+            A string to send to node TCP server, could be a JSON dumps...
+        :return: None
+        """
+        if self.verbose:
+            print '<- out ' + msg
+        self._socket.send(msg)
+        return
+
+    def recv(self):
+        """
+        Checks the ZeroMQ for data
+        :return: str
+            String of data
+        """
+        return self._socket.recv()
+
+
+class RingBuffer(np.ndarray):
+    """A multidimensional ring buffer."""
+
+    def __new__(cls, input_array):
+        obj = np.asarray(input_array).view(cls)
+        return obj
+
+    def __array_finalize__(self, obj):
+        if obj is None:
+            return
+
+    def __array_wrap__(self, out_arr, context=None):
+        return np.ndarray.__array_wrap__(self, out_arr, context)
+
+    def append(self, x):
+        """Adds element x to the ring buffer."""
+        x = np.asarray(x)
+        self[:, :-1] = self[:, 1:]
+        self[:, -1] = x
+
+class Stream:
+    """
+    """
+    def __init__(self, port=3000, buffer_size=2500, nb_chans=8, verbose=False):
+
+        # port
+        self.port = port
+
+        #  size
+        self.buffer_size = buffer_size
+
+        # create a new python interface.
+        self.interface = Interface(port=port, verbose=verbose)
+
+        self.signal = RingBuffer(np.zeros((nb_chans + 1, buffer_size)))
+
+        self.start = self.start
+
+    def start(self):
+
+        while True:
+            msg = self.interface.recv()
+            try:
+                dicty = json.loads(msg)
+                action = dicty.get('action')
+                command = dicty.get('command')
+                message = dicty.get('message')
+
+                if command == 'sample':
+                    if action == 'process':
+                        # Do sample processing here
+                        try:
+                            if type(message) is not dict:
+                                print "sample is not a dict", message
+                                raise ValueError
+                            # Get keys of sample
+                            data = np.zeros(9)
+
+                            data[:-1] = message.get('channelData')
+                            data[-1] = message.get('timeStamp')
+
+                            # Add data to end of ring buffer
+                            self.signal.append(data)
+
+                            print(data)
+                            print message.get('sampleNumber', data)
+
+                        except ValueError as e:
+                            print e
+                elif command == 'status':
+                    if action == 'active':
+                        interface.send(json.dumps({
+                            'action': 'alive',
+                            'command': 'status',
+                            'message': time.time() * 1000.0
+                        }))
+
+            except BaseException as e:
+                print e
